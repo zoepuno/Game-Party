bc7886fc1957ae4627de27dd384607558b8d71dc
zoepuno <98069599+zoepuno@users.noreply.github.com>
2022-01-20 09:25:11 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,69 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>gameparty</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
+  </head>
+<body>
+  <header>
+    <nav id="header-nav" class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="index.html" class="pull-left visible-md visible-lg">
+            <div id="logo-img"></div>
+          </a>
+
+          <div class="navbar-brand">
+            <a href="index.html"><h1>gameparty</h1></a>
+            <p>
+              
+              <span>Rated #1 Best Kids Game</span>
+            </p>
+          </div>
+
+          <button id="navbarToggle" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsable-nav" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+        </div>
+        
+        <div id="collapsable-nav" class="collapse navbar-collapse">
+           <ul id="nav-list" class="nav navbar-nav navbar-right">
+            <li class="visible-xs active">
+              <a href="index.html">
+                <span class="glyphicon glyphicon-home"></span> Home</a>
+            </li>
+            <li>
+              <a href="accounts.html">
+                <span class="#"></span><br class="hidden-xs"> ABC Games</a>
+            </li>
+            <li>
+              <a href="accounts.html">
+                <span class="#"></span><br class="hidden-xs"> Math Games</a>
+            </li>
+            <li>
+              <a href="accounts.html">
+                <span class="#"></span><br class="hidden-xs"> Cool Games!</a>
+            </li>
+            <li>
+              <a href="accounts.html">
+                <span class="#"></span><br class="hidden-xs"> Art Games</a>
+            </li>
+            <li>
+              <a href="accounts.html">
+                <span class="#"></span><br class="hidden-xs"> Accounts</a>
+            </li>
+           
+          
+          </ul><!-- #nav-list -->
+        </div><!-- .collapse .navbar-collapse -->
+      </div><!-- .container -->
+    </nav><!-- #header-nav -->
+  </header>
