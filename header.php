5ff2346b4017b5dcd71ac59216abde642d977c0d
christinegoh1 <89264368+christinegoh1@users.noreply.github.com>
2022-03-09 09:34:31 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,8 @@
+<?php
+  session_start();
+
+?>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -5,26 +10,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>gameparty</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
+       
+
     <link rel="stylesheet" href="css/styles.css">
-    <link href='https://fonts.googleapis.com/css?family=Oxygen:400,300,700' rel='stylesheet' type='text/css'>
-    <link href='https://fonts.googleapis.com/css?family=Lora' rel='stylesheet' type='text/css'>
-  </head>
+    
+ </head>
 <body>
   <header>
     <nav id="header-nav" class="navbar navbar-default">
       <div class="container">
         <div class="navbar-header">
-          <a href="index.html" class="pull-left visible-md visible-lg">
+          <a href="index.php" class="pull-left visible-md visible-lg">
             <div id="logo-img"></div>
           </a>
 
-          <div class="navbar-brand">
-            <a href="index.html"><h1>gameparty</h1></a>
-            <p>
-              
-              <span>Rated #1 Best Kids Game</span>
-            </p>
-          </div>
+          
 
           <button id="navbarToggle" type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapsable-nav" aria-expanded="false">
             <span class="sr-only">Toggle navigation</span>
@@ -36,34 +36,67 @@
         
         <div id="collapsable-nav" class="collapse navbar-collapse">
            <ul id="nav-list" class="nav navbar-nav navbar-right">
-            <li class="visible-xs active">
-              <a href="index.html">
-                <span class="glyphicon glyphicon-home"></span> Home</a>
+            <li >
+              <a href="index.php">
+                <span class="#"></span><br class="hidden-xs">Home</a>
             </li>
             <li>
-              <a href="accounts.html">
+              <a href="alphabetgames.php">
                 <span class="#"></span><br class="hidden-xs"> ABC Games</a>
             </li>
             <li>
-              <a href="accounts.html">
+              <a href="mathgames.php">
                 <span class="#"></span><br class="hidden-xs"> Math Games</a>
             </li>
             <li>
-              <a href="accounts.html">
-                <span class="#"></span><br class="hidden-xs"> Cool Games!</a>
-            </li>
-            <li>
-              <a href="accounts.html">
+              <a href="artgames.php">
                 <span class="#"></span><br class="hidden-xs"> Art Games</a>
             </li>
             <li>
-              <a href="accounts.html">
-                <span class="#"></span><br class="hidden-xs"> Accounts</a>
+              <a href="logicgames.php">
+                <span class="#"></span><br class="hidden-xs"> Logic Games</a>
             </li>
+
+
+            <?php 
+            if(!isset($_SESSION["user_id"]))
+            {
+              echo"<li>
+              <a href='login.php'>
+                <span class='#'></span><br class='hidden-xs'> Login/Sign Up</a>
+              </li>";
+              
+            }
+            else
+            {
+               echo "<li>
+                <a href='welcome.php'>
+                  <span class='#'></span><br class='hidden-xs'> My Profile</a>
+              </li>
+               <li>
+                <a href='logout.php'>
+                  <span class='#'></span><br class='hidden-xs'> Logout</a>
+              </li>";
+            }
+            
+            ?>
+              <!--/*if (isset($_SESSION["useruid"])){
+                  echo "<li><a href='profile.php'>Profile page</a></li>";
+                  echo "<li><a href='includes/logout.inc.php'>Log out</a></li>";
+              }
+              else{
+                  echo "<li><a href='signup.php'>Sign Up</a></li>";
+                  echo "<li><a href='login.php'>Log in</a></li>";
+              }*/
+            ?>-->
+          
            
           
           </ul><!-- #nav-list -->
         </div><!-- .collapse .navbar-collapse -->
       </div><!-- .container -->
+
     </nav><!-- #header-nav -->
+
   </header>
+  <script src="js/app.js"></script>
