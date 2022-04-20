cd6874cef452ee3c1d27236025d0933f623c6b4e
christinegoh1 <89264368+christinegoh1@users.noreply.github.com>
2022-04-20 09:07:52 -0500

    Add files via upload

---
+++ paint.php
@@ -0,0 +1,23 @@
+<?php
+      include_once 'header.php';
+      include 'config.php';
+?>
+
+<!DOCTYPE html>
+<link rel="stylesheet" href="css/paint.css">
+<head>
+<meta charset="utf-8" />
+<title>Paint</title>
+<script type="text/JavaScript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js?ver=1.4.2"></script>
+</head>
+<body>
+	<canvas id="myCanvas">
+		
+	</canvas>
+</body>
+</html>
+
+<script src="js/jquery-2.1.4.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/paint.js"></script>
+  
